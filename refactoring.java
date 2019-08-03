00d6af2b39a7facd19becb753638f0c8b01bb838
HFYEH <sharefun010407@gmail.com>
2019-08-03 19:51:37 +0800

    Replace type code with state/strategy

--- refactoring.java
+++ refactoring.java
@@ -1,10 +1,32 @@
+abstract class Price {
+    abstract int getPriceCode();
+}
+
+class ChildrensPrice extends Price {
+    int getPriceCode(){
+        return Movie.CHILDRENS;
+    }
+}
+
+class NewReleasePrice extends Price {
+    int getPriceCode(){
+        return Movie.NEW_RELEASE;
+    }
+}
+
+class RegularPrice extends Price {
+    int getPriceCode(){
+        return Movie.Regular;
+    }
+}
+
 public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     
     private String _title;
-    private int _priceCode;
+    private Price _price;
     
     public Movie(String title, int priceCode) {
         _title = title;
@@ -12,11 +34,23 @@ public class Movie {
     }
     
     public int getPriceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
     
     public void setPriceCode(int arg) {
-        _priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
     
     public String getTitle() {
