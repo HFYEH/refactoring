66a0c6d845d1acf9de4f7d6f5c96f56d3581d2c6
HFYEH <sharefun010407@gmail.com>
2019-08-03 20:32:29 +0800

    Replace conditional with polymorphism

--- refactoring.java
+++ refactoring.java
@@ -1,10 +1,22 @@
 abstract class Price {
     abstract int getPriceCode();
+    abstract double getCharge(daysRented);
+
+    int getFrequentRenterPoints(daysRented) {
+        return 1;
+    }
 }
 
 class ChildrensPrice extends Price {
     int getPriceCode(){
         return Movie.CHILDRENS;
+    }
+    
+    double getCharge(daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+        return result;
     }
 }
 
@@ -12,11 +24,26 @@ class NewReleasePrice extends Price {
     int getPriceCode(){
         return Movie.NEW_RELEASE;
     }
+
+    double getCharge(daysRented) {
+        return daysRented * 3;
+    }
+    
+    int getFrequentRenterPoints(daysRented) {
+        return (daysRented > 1) ? 2 : 1;
+    }
 }
 
 class RegularPrice extends Price {
     int getPriceCode(){
         return Movie.Regular;
+    }
+
+    double getCharge(daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
     }
 }
 
@@ -58,30 +85,11 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double result = 0;
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-        else
-            return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
 
